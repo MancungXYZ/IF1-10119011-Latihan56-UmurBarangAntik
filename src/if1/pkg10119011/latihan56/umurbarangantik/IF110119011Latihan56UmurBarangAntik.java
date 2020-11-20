5d2b36b24c0c4ed7fef87e2ad27fb5943368b7f4
Reihan Wiyanda <Reihan Wiyanda@Mancungs>
2020-11-20 13:21:32 +0700

    IF1-10119011-Latihan56-UmurBarangAntik

---
+++ src/if1/pkg10119011/latihan56/umurbarangantik/IF110119011Latihan56UmurBarangAntik.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119011.latihan56.umurbarangantik;
+
+/**
+ *
+ * @author 
+ *  Nama    : Reihan Wiyanda
+ */
+public class IF110119011Latihan56UmurBarangAntik {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Radio rdo = new Radio(234);
+        rdo.setName("Radio AM");
+        System.out.println("Nama Barang Antik : " + rdo.getName());
+        rdo.tampilUmur();
+    }
+    
+}
